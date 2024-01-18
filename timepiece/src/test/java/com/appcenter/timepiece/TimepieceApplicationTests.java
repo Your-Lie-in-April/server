574e90813f8fe4d3b92d587a429c611d15296273
김정아 <92552047+squidjiny@users.noreply.github.com>
2024-01-19 05:46:16 +0900

    [Feat] 서버 기본 파일 업로드

---
+++ timepiece/src/test/java/com/appcenter/timepiece/TimepieceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appcenter.timepiece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimepieceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
