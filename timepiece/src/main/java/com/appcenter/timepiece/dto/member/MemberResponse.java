b61cd193720c97a04dbc8d502e33297f893caf36
jang-namu <ironhee8005@gmail.com>
2024-02-05 18:28:57 +0900

    style: 줄바꿈

--- timepiece/src/main/java/com/appcenter/timepiece/dto/member/MemberResponse.java
+++ timepiece/src/main/java/com/appcenter/timepiece/dto/member/MemberResponse.java
@@ -6,9 +6,13 @@ import lombok.Getter;
 public class MemberResponse {
 
     private Long memberId;
+
     private String email;
+
     private String nickname;
+
     private String state;
+
     private String profileImageUrl;
 
     public MemberResponse(Long memberId, String email, String nickname, String state, String profileImageUrl) {
--- timepiece/src/main/java/com/appcenter/timepiece/dto/schedule/ScheduleDayResponse.java
+++ timepiece/src/main/java/com/appcenter/timepiece/dto/schedule/ScheduleDayResponse.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class ScheduleDayResponse {
 
     private String daysOfWeek;
+
     private List<ScheduleResponse> schedule;
 
     public ScheduleDayResponse(String daysOfWeek, List<ScheduleResponse> schedule) {
