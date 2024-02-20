11c3211f002cc7196160487e5bebd2a8c567e141
jang-namu <ironhee8005@gmail.com>
2024-02-20 17:45:35 +0900

    init: CommonResponse

---
+++ timepiece/src/main/java/com/appcenter/timepiece/common/dto/CommonResponse.java
@@ -0,0 +1,19 @@
+package com.appcenter.timepiece.common.dto;
+
+
+import lombok.Builder;
+
+public class CommonResponse<T> {
+
+    private String status;
+
+    private String message;
+
+    private T data;
+
+    public CommonResponse(String status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+}
