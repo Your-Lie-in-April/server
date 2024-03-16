4df4860ffa9abb6e7263202975282d491eb7bb91
jang-namu <ironhee8005@gmail.com>
2024-03-16 17:12:50 +0900

    feat: corsConfigurer, swagger 서버 definition * corsConfigurer 우선 모든 오리진을 허용(추후 수정 필요) * swagger HTTPS 요청이 정상적으로 전달될 수 있도록 Definition 작성

--- timepiece/src/main/java/com/appcenter/timepiece/TimepieceApplication.java
+++ timepiece/src/main/java/com/appcenter/timepiece/TimepieceApplication.java
@@ -1,10 +1,13 @@
 package com.appcenter.timepiece;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server url")})
 @EnableJpaAuditing
 @SpringBootApplication
 public class TimepieceApplication {
