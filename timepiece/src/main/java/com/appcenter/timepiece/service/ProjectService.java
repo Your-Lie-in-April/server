3b8b2b1d500de74e53a6c8cb912db6646f95aade
jang-namu <ironhee8005@gmail.com>
2024-03-23 23:05:25 +0900

    feat: 프로젝트 구성원 조회는 자신이 속한 프로젝트에서만 가능하다.

--- timepiece/src/main/java/com/appcenter/timepiece/service/ProjectService.java
+++ timepiece/src/main/java/com/appcenter/timepiece/service/ProjectService.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
-
 import static java.util.Objects.requireNonNull;
 
 @Slf4j
