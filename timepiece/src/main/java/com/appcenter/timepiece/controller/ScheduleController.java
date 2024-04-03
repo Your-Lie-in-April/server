528e0461cea7006156911c4510f4450458b92d5a
jang-namu <ironhee8005@gmail.com>
2024-04-04 00:20:17 +0900

    feat: 스케줄 수정, 삭제, 조회(프로젝트 단위, 개인단위) 기능 추가

--- timepiece/src/main/java/com/appcenter/timepiece/controller/ScheduleController.java
+++ timepiece/src/main/java/com/appcenter/timepiece/controller/ScheduleController.java
@@ -11,29 +11,106 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class ScheduleController {
 
     private final ScheduleService scheduleService;
 
-    //현재 ScheduleCreateUpdateRequest에서 projectId를 이미 받고있는데 @pathvariable로 또 받고있습니다. 수정이 필요해보입니다.
+    /**
+     * For Test During Develop
+     *
+     * @return CommonResponse<List < ScheduleWeekResponse>>
+     */
+    @GetMapping("/v1/schedules/all")
+    public CommonResponse<?> findAllSchedules() {
+        return CommonResponse.success("성공", null);
+    }
+
+    /**
+     * {@summary 인증 성공 시 프로젝트 내 모든 사용자 시간표 조회}
+     * <p>프로젝트에 속해있는 모든 사용자의 시간표 정보{@literal ((List<ScheduleWeekResponse>) 반환}
+     *
+     * @param projectId   프로젝트 식별자
+     * @param userDetails JWT 인증 시, SecurityContext에 저장된 사용자 정보
+     * @return {@literal List<ScheduleWeekResponse>}
+     */
+    @GetMapping("/v1/projects/{projectId}/schedules")
+    public CommonResponse<List<?>> findMembersSchedules(@PathVariable Long projectId,
+                                                        @RequestParam LocalDate condition,
+                                                        @AuthenticationPrincipal UserDetails userDetails) {
+        return CommonResponse.success("성공", scheduleService.findMembersSchedules(projectId, condition, userDetails));
+    }
+
+    /**
+     * {@summary 인증 성공 시, 특정 프로젝트-특정 사용자의 시간표 조회}
+     * <p> projectId에 속하는 프로젝트 내에서 memberId와 일치하는 사용자의 일주일 스케줄을 조회한다.
+     * 속해있지 않은 프로젝트에 작성된 스케줄은 조회하지 못하고, 내가 현재 속해있는 프로젝트 안에서만 사용가능하다.
+     *
+     * @param projectId   프로젝트 식별자
+     * @param memberId    멤버 식별자
+     * @param userDetails JWT 인증 시, SecurityContext에 저장된 사용자 정보
+     * @return {@literal CommmonResponse<ScheduleWeekResponse>}
+     */
+    @GetMapping("/v1/projects/{projectId}/members/{memberId}/schedules")
+    public CommonResponse<?> findSchedule(@PathVariable Long projectId,
+                                          @PathVariable Long memberId,
+                                          @RequestParam LocalDate condition,
+                                          @AuthenticationPrincipal UserDetails userDetails) {
+        return CommonResponse.success("성공", scheduleService.findSchedule(projectId, memberId, condition, userDetails));
+    }
+
+    /**
+     * {@summary 인증이 성공하면 request에 맞춰 스케줄을 생성한다.}
+     * <p>
+     *
+     * @param request     스케줄 생성 및 삭제 요청 시, 사용되는 DTO(ScheduleCreateUpdateRequest)
+     * @param projectId   프로젝트 식별자
+     * @param userDetails JWT 인증 시, SecurityContext에 저장된 사용자 정보
+     * @return
+     */
     @PostMapping("/v1/projects/{projectId}/schedules")
-    public ResponseEntity<CommonResponse<?>> createSchedule(@RequestBody ScheduleCreateUpdateRequest request
-            , @PathVariable Long projectId, @AuthenticationPrincipal UserDetails userDetails) {
+    public ResponseEntity<CommonResponse<?>> createSchedule(@PathVariable Long projectId,
+                                                            @RequestBody ScheduleCreateUpdateRequest request,
+                                                            @AuthenticationPrincipal UserDetails userDetails) {
         scheduleService.createSchedule(request, projectId, userDetails);
         return ResponseEntity.status(HttpStatus.CREATED).body(CommonResponse.success("성공", null));
     }
 
+    /**
+     * {@summary 인증이 성공하면 request에 맞춰 스케줄을 수정한다.}
+     * <p>
+     *
+     * @param request     스케줄 생성 및 삭제 요청 시, 사용되는 DTO(ScheduleCreateUpdateRequest)
+     * @param projectId   프로젝트 식별자
+     * @param userDetails JWT 인증 시, SecurityContext에 저장된 사용자 정보
+     * @return
+     */
     @PutMapping("/v1/projects/{projectId}/schedules")
-    public ResponseEntity<CommonResponse<?>> updateSchedule(@RequestBody ScheduleCreateUpdateRequest request
-            , @PathVariable Long projectId, @AuthenticationPrincipal UserDetails userDetails) {
-        return null;
+    public ResponseEntity<CommonResponse<?>> updateSchedule(@PathVariable Long projectId,
+                                                            @RequestBody ScheduleCreateUpdateRequest request,
+                                                            @AuthenticationPrincipal UserDetails userDetails) {
+        scheduleService.editSchedule(request, projectId, userDetails);
+        return ResponseEntity.ok().body(CommonResponse.success("성공", null));
     }
 
+    /**
+     * {@summary 인증이 성공하면 request에 맞춰 스케줄을 삭제한다.}
+     * <p>
+     *
+     * @param request
+     * @param projectId   프로젝트 식별자
+     * @param userDetails JWT 인증 시, SecurityContext에 저장된 사용자 정보
+     * @return
+     */
     @DeleteMapping("/v1/projects/{projectId}/schedules")
-    public ResponseEntity<CommonResponse<?>> deleteSchedule(@RequestBody ScheduleDeleteRequest request
-            , @PathVariable Long projectId, @AuthenticationPrincipal UserDetails userDetails) {
-        return null;
+    public ResponseEntity<CommonResponse<?>> deleteSchedule(@PathVariable Long projectId,
+                                                            @RequestBody ScheduleDeleteRequest request,
+                                                            @AuthenticationPrincipal UserDetails userDetails) {
+        scheduleService.deleteSchedule(request, projectId, userDetails);
+        return ResponseEntity.ok().body(CommonResponse.success("성공", null));
     }
 }
--- timepiece/src/main/java/com/appcenter/timepiece/service/ScheduleService.java
+++ timepiece/src/main/java/com/appcenter/timepiece/service/ScheduleService.java
@@ -1,11 +1,12 @@
 package com.appcenter.timepiece.service;
 
 import com.appcenter.timepiece.common.exception.ExceptionMessage;
+import com.appcenter.timepiece.common.exception.NotEnoughPrivilegeException;
 import com.appcenter.timepiece.common.exception.NotFoundElementException;
 import com.appcenter.timepiece.common.security.CustomUserDetails;
 import com.appcenter.timepiece.domain.MemberProject;
 import com.appcenter.timepiece.domain.Schedule;
-import com.appcenter.timepiece.dto.schedule.ScheduleCreateUpdateRequest;
+import com.appcenter.timepiece.dto.schedule.*;
 import com.appcenter.timepiece.repository.MemberProjectRepository;
 import com.appcenter.timepiece.repository.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +15,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,29 +30,150 @@ public class ScheduleService {
 
     private final MemberProjectRepository memberProjectRepository;
 
+    /**
+     * {@summary 프로젝트 내 모든 멤버의 스케줄을 조회한다(본인포함)}
+     * <p>자신을 포함한 모든 멤버의 스케줄을 조회한다.
+     * 모든 프로젝트 멤버의 스케줄을 전체 조회한 후,스케줄 중 멤버별로 중복되는 요일을 필터링,
+     * 자신의 스케줄을 필터링하여 요일별로 묶어 반환한다. </p>
+     * @param projectId
+     * @param condition
+     * @param userDetails
+     * @return
+     */
+    @Transactional(readOnly = true)
+    public List<ScheduleWeekResponse> findMembersSchedules(Long projectId, LocalDate condition, UserDetails userDetails) {
+        validateMemberIsInProject(projectId, userDetails);
+        List<MemberProject> memberProjects = memberProjectRepository.findAllByProjectId(projectId);
+        LocalDateTime sundayOfWeek = calculateStartDay(LocalDateTime.of(condition, LocalTime.MIN));
+
+        List<Schedule> schedules = scheduleRepository.findMembersWeekSchedule(memberProjects.stream().map(MemberProject::getId).toList(), sundayOfWeek, sundayOfWeek.plusDays(7));
+        return memberProjects.stream().map(memberProject ->
+                new ScheduleWeekResponse(memberProject.getMember().getNickname(),
+                schedules.stream()
+                        .filter(schedule -> schedule.getMemberProject().getId().equals(memberProject.getId()))
+                        .map(schedule -> schedule.getStartTime().getDayOfWeek())
+                        .distinct()
+                        .map(dayOfWeek -> ScheduleDayResponse.of(dayOfWeek,
+                                schedules.stream()
+                                        .filter(schedule -> schedule.getMemberProject().getId().equals(memberProject.getId()))
+                                        .filter(schedule -> schedule
+                                                .getStartTime()
+                                                .getDayOfWeek()
+                                                .equals(dayOfWeek))
+                                        .map(ScheduleDto::from)
+                                        .collect(Collectors.toList())))
+                        .collect(Collectors.toList())))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * {@summary 특정 프로젝트-사용자의 condition 날짜가 포함된 주차 스케줄 조회}
+     * <p>condition이 속한 주차(= 해당 주차의 일요일 ~ 토요일까지)의 사용자 스케줄을 조회한다.
+     * 해당 기간동안의 사용자의 스케줄을 전부 조회한 후 날짜별로 묶어 반환한다.
+     * @param projectId
+     * @param memberId
+     * @param condition
+     * @param userDetails
+     * @return
+     */
+    @Transactional(readOnly = true)
+    public ScheduleWeekResponse findSchedule(Long projectId, Long memberId, LocalDate condition, UserDetails userDetails) {
+        validateMemberIsInProject(projectId, userDetails);
+
+        MemberProject memberProject = memberProjectRepository.findByMemberIdAndProjectId(memberId, projectId)
+                .orElseThrow(() -> new NotFoundElementException(ExceptionMessage.MEMBER_PROJECT_NOT_FOUND));
+        LocalDateTime sundayOfWeek = calculateStartDay(LocalDateTime.of(condition, LocalTime.MIN));
+
+        List<Schedule> schedules = scheduleRepository.findMemberWeekSchedule(memberProject.getId(), sundayOfWeek, sundayOfWeek.plusDays(7));
+        return new ScheduleWeekResponse(memberProject.getMember().getNickname(), schedules.stream()
+                .map(schedule -> schedule.getStartTime().getDayOfWeek())
+                .distinct()
+                .map(dayOfWeek -> ScheduleDayResponse.of(dayOfWeek, schedules.stream()
+                        .filter(schedule -> schedule
+                                .getStartTime()
+                                .getDayOfWeek()
+                                .equals(dayOfWeek))
+                        .map(ScheduleDto::from)
+                        .collect(Collectors.toList())))
+                .collect(Collectors.toList()));
+    }
+
+    // todo: ProjectService와 중복코드
+    private void validateMemberIsInProject(Long projectId, UserDetails userDetails) {
+        Long memberId = ((CustomUserDetails) userDetails).getId();
+        boolean isExist = memberProjectRepository.existsByMemberIdAndProjectId(memberId, projectId);
+        if (!isExist) {
+            throw new NotEnoughPrivilegeException("속하지 않은 프로젝트 정보를 조회할 수 없습니다.");
+        }
+    }
+
+    /**
+     * {@summary 스케줄 생성}
+     * @param request
+     * @param projectId
+     * @param userDetails
+     */
     @Transactional
     public void createSchedule(ScheduleCreateUpdateRequest request, Long projectId, UserDetails userDetails) {
         Long memberId = ((CustomUserDetails) userDetails).getId();
-
         MemberProject memberProject = memberProjectRepository.findByMemberIdAndProjectId(memberId, projectId)
                 .orElseThrow(() -> new NotFoundElementException(ExceptionMessage.MEMBER_PROJECT_NOT_FOUND));
 
         List<Schedule> schedulesToSave = request.getSchedule().stream()
                 .flatMap(scheduleDayRequest -> scheduleDayRequest.getSchedule().stream())
                 .map(scheduleDto -> Schedule.of(scheduleDto, memberProject))
-                .collect(Collectors.toList());
+                .toList();
 
         scheduleRepository.saveAll(schedulesToSave);
     }
 
+    /**
+     * {@summary 기존 스케줄 삭제 후 새 스케줄 저장}
+     * <p>request에 존재하는 첫번째 스케줄의 날짜로, 해당 주차의 첫번째 요일(일요일)을 계산,
+     * 계산한 첫번째 요일 ~ (첫번째 요일 + 7)일의 기간에 속하는 스케줄을 DB에서 삭제하고 request로 받은
+     * 수정 후 스케줄을 저장
+     * @param request
+     * @param projectId
+     * @param userDetails
+     */
     public void editSchedule(ScheduleCreateUpdateRequest request, Long projectId, UserDetails userDetails) {
         Long memberId = ((CustomUserDetails) userDetails).getId();
+        MemberProject memberProject = memberProjectRepository.findByMemberIdAndProjectId(memberId, projectId)
+                .orElseThrow(() -> new NotFoundElementException(ExceptionMessage.MEMBER_PROJECT_NOT_FOUND));
 
+        // todo: IndexOutOfBoundsException!! 발생 가능
+        LocalDateTime sundayOfWeek = calculateStartDay(request.getSchedule().get(0).getSchedule().get(0).getStartTime());
+        scheduleRepository.deleteMemberSchedulesBetween(memberProject.getId(), sundayOfWeek, sundayOfWeek.plusDays(7));
+
+        List<Schedule> schedulesToSave = request.getSchedule().stream()
+                .flatMap(scheduleDayRequest -> scheduleDayRequest.getSchedule().stream())
+                .map(scheduleDto -> Schedule.of(scheduleDto, memberProject))
+                .toList();
+
+        scheduleRepository.saveAll(schedulesToSave);
     }
 
-    public void deleteSchedule(ScheduleCreateUpdateRequest request, Long projectId, UserDetails userDetails) {
-        Long memberId = ((CustomUserDetails) userDetails).getId();
+    // todo: ProjectService와 중복코드
+    private LocalDateTime calculateStartDay(LocalDateTime condition) {
+        return condition.minusDays(condition.getDayOfWeek().getValue() % 7);
+    }
 
+
+    /**
+     * {@summary start <= 날짜 < end에 속하는 스케줄 삭제}
+     * @param request
+     * @param projectId
+     * @param userDetails
+     */
+    public void deleteSchedule(ScheduleDeleteRequest request, Long projectId, UserDetails userDetails) {
+        Long memberId = ((CustomUserDetails) userDetails).getId();
+        MemberProject memberProject = memberProjectRepository.findByMemberIdAndProjectId(memberId, projectId)
+                .orElseThrow(() -> new NotFoundElementException(ExceptionMessage.MEMBER_PROJECT_NOT_FOUND));
+
+        // todo: 현재 endDate 날짜에 속하는 스케줄은 삭제되지 않음
+        scheduleRepository.deleteMemberSchedulesBetween(memberProject.getId(),
+                LocalDateTime.of(request.getStartDate(), LocalTime.MIN),
+                LocalDateTime.of(request.getEndDate(), LocalTime.MIN));
     }
 
 }
