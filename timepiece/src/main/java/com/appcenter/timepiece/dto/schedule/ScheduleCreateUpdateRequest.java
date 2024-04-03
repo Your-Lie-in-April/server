b2a0499ecff5792d89bdd64b18327ff079976314
jang-namu <ironhee8005@gmail.com>
2024-04-03 18:14:03 +0900

    docs: ScheduleCreateUpdateRequest.schedule, 스웨거 example 수정

--- timepiece/src/main/java/com/appcenter/timepiece/dto/schedule/ScheduleCreateUpdateRequest.java
+++ timepiece/src/main/java/com/appcenter/timepiece/dto/schedule/ScheduleCreateUpdateRequest.java
@@ -12,24 +12,57 @@ import java.util.List;
 @Schema(description = "스케줄 생성 및 수정 요청", requiredProperties = {"projectId", "schedule"})
 public class ScheduleCreateUpdateRequest {
 
-    @Schema(description = "주 단위 스케줄", example = "[" +
-            "{" +
-            "\"schedule\" : [{\"startTime\": \"2024-01-31T17:00:00+09:00\", \"endTime\": \"2024-01-31T20:30:00+09:00\"}," +
-            "{\"startTime\": \"2024-01-31T21:30:00+09:00\", \"endTime\": \"2024-01-31T22:30:00+09:00\"}]" +
-            "}," +
-            "{" +
-            "\"schedule\" : [{\"startTime\": \"2024-02-1T10:30:00+09:00\", \"endTime\":\"2024-02-1T11:30:00+09:00\" }]" +
-            "}," +
-            "{" +
-            "\"schedule\" : [{\"startTime\": \"2024-02-2T10:30:00+09:00\", \"endTime\":\"2024-02-2T11:30:00+09:00\" }]" +
-            "}," +
-            "{\n" +
-            "\"schedule\" : [{\"startTime\": \"2024-02-3T10:30:00+09:00\", \"endTime\":\"2024-02-3T11:30:00+09:00\" }]" +
-            "}," +
-            "{" +
-            "\"schedule\" : [{\"startTime\": \"2024-02-4T10:30:00+09:00\", \"endTime\":\"2024-02-4T11:30:00+09:00\" }]" +
-            "}" +
-            "]")
+    private Long projectId;
+
+    @Schema(description = "주 단위 스케줄", example =
+            """
+              [
+                {
+                  "schedule": [
+                    {
+                      "startTime": "2024-01-31T17:00:00",
+                      "endTime": "2024-01-31T20:30:00"
+                    },
+                    {
+                      "startTime": "2024-01-31T21:30:00",
+                      "endTime": "2024-01-31T22:30:00"
+                    }
+                  ]
+                },
+                {
+                  "schedule": [
+                    {
+                      "startTime": "2024-02-01T10:30:00",
+                      "endTime": "2024-02-01T11:30:00"
+                    }
+                  ]
+                },
+                {
+                  "schedule": [
+                    {
+                      "startTime": "2024-02-02T10:30:00",
+                      "endTime": "2024-02-02T11:30:00"
+                    }
+                  ]
+                },
+                {
+                  "schedule": [
+                    {
+                      "startTime": "2024-02-03T10:30:00",
+                      "endTime": "2024-02-03T11:30:00"
+                    }
+                  ]
+                },
+                {
+                  "schedule": [
+                    {
+                      "startTime": "2024-02-04T10:30:00",
+                      "endTime": "2024-02-04T11:30:00"
+                    }
+                  ]
+                }
+              ]
+            """)
     private List<ScheduleDayRequest> schedule;
 
 }
