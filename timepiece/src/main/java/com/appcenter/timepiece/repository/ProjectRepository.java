3ada472b772f4973be5be5bf40fbfdd5fb497533
jang-namu <ironhee8005@gmail.com>
2024-05-01 20:47:23 +0900

    fix: findProjectByMemberIdAndTitleLikeKeyword 올바른 리턴 타입으로 수정

--- timepiece/src/main/java/com/appcenter/timepiece/repository/ProjectRepository.java
+++ timepiece/src/main/java/com/appcenter/timepiece/repository/ProjectRepository.java
@@ -12,14 +12,14 @@ import java.util.List;
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
-    @Query("select p from Project p left join fetch p.cover c")
+    @Query("select p from Project p left  join fetch p.cover c")
     List<Project> findAllWithCover();
 
     @Query("select distinct p from Project p " +
             "join p.memberProjects mp " +
             "join mp.member m " +
             "where m.id = :memberId and p.title like %:keyword% and mp.isStored = false ")
-    List<Project> findProjectByMemberIdAndTitleLikeKeyword(Pageable pageable, Long memberId, String keyword);
+    Page<Project> findProjectByMemberIdAndTitleLikeKeyword(Pageable pageable, Long memberId, String keyword);
 
     @Query("select distinct p from Project p " +
             "join p.memberProjects mp " +
