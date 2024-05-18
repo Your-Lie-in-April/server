81860878cce076b91a894de7e46ecbec5a27fe90
김정아 <92552047+squidjiny@users.noreply.github.com>
2024-05-18 20:11:56 +0900

    refactor: refreshToken reissue 메서드 GET->POST로 변경#50

--- timepiece/src/main/java/com/appcenter/timepiece/controller/AuthController.java
+++ timepiece/src/main/java/com/appcenter/timepiece/controller/AuthController.java
@@ -10,9 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +22,7 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @GetMapping(value = "/v1/auth/reissue")
+    @PostMapping(value = "/v1/auth/reissue")
     @Operation(summary = "토큰 재발급", description = "")
     @SwaggerApiResponses
     public ResponseEntity<CommonResponse> reissueAccessToken(HttpServletRequest request) {
