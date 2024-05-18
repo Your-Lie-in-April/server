b58bf1c22aa05325c009c0c60ff57017c39c5ba2
jang-namu <ironhee8005@gmail.com>
2024-05-18 20:53:45 +0900

    feat: redisTemplate Object Serailizer

--- timepiece/src/main/java/com/appcenter/timepiece/common/redis/RedisConfig.java
+++ timepiece/src/main/java/com/appcenter/timepiece/common/redis/RedisConfig.java
@@ -1,11 +1,16 @@
 package com.appcenter.timepiece.common.redis;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -24,5 +29,21 @@ public class RedisConfig {
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
         config.setPassword(password);
         return new LettuceConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+
+        Jackson2JsonRedisSerializer<Object> valueSerializer = new
+                Jackson2JsonRedisSerializer<>(mapper, Object.class);
+        redisTemplate.setValueSerializer(valueSerializer);
+
+        return redisTemplate;
     }
 }
