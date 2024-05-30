4cc62ffca827fa12250b7f1a3281bf3ebed114d1
김정아 <92552047+squidjiny@users.noreply.github.com>
2024-05-30 20:30:03 +0900

    feat: projectRepository에 querydsl 적용 완료#80

--- timepiece/src/main/java/com/appcenter/timepiece/repository/customRepository/CustomProjectRepository.java
+++ timepiece/src/main/java/com/appcenter/timepiece/repository/customRepository/CustomProjectRepository.java
@@ -1,4 +1,11 @@
 package com.appcenter.timepiece.repository.customRepository;
 
+import com.appcenter.timepiece.domain.Project;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface CustomProjectRepository {
+
+    Page<Project> findProject(Long memberId, String keyword, Boolean isStored, Boolean isDeleted, Pageable pageable);
+
 }
