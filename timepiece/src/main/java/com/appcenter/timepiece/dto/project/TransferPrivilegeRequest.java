da71239d8cb9c3a01b79937a86b5bf6cb5081df6
jang-namu <ironhee8005@gmail.com>
2024-06-01 22:54:58 +0900

    fix: 기본 생성자 부재로 인한 역직렬화 실패 문제 해결

--- timepiece/src/main/java/com/appcenter/timepiece/dto/project/TransferPrivilegeRequest.java
+++ timepiece/src/main/java/com/appcenter/timepiece/dto/project/TransferPrivilegeRequest.java
@@ -1,8 +1,11 @@
 package com.appcenter.timepiece.dto.project;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TransferPrivilegeRequest {
     Long toMemberId;
 
