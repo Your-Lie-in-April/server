cc732882a2cdf21ac00a28b1d3d8640e0d47baf6
김정아 <92552047+squidjiny@users.noreply.github.com>
2024-07-23 02:08:19 +0900

    feat: 프로젝트 스케줄 생성에서 스케줄이 빈 배열인지 확인하는 유효성 검사 추가#119

--- timepiece/src/main/java/com/appcenter/timepiece/dto/schedule/ScheduleCreateUpdateRequest.java
+++ timepiece/src/main/java/com/appcenter/timepiece/dto/schedule/ScheduleCreateUpdateRequest.java
@@ -1,6 +1,7 @@
 package com.appcenter.timepiece.dto.schedule;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,45 +15,46 @@ public class ScheduleCreateUpdateRequest {
 
     @Schema(description = "주 단위 스케줄", example =
             """
-              [
-                {
-                  "schedule": [
-                    {
-                      "startTime": "2024-01-31T17:00:00",
-                      "endTime": "2024-01-31T20:30:00"
-                    },
-                    {
-                      "startTime": "2024-01-31T21:30:00",
-                      "endTime": "2024-01-31T22:00:00"
-                    }
-                  ]
-                },
-                {
-                  "schedule": [
-                    {
-                      "startTime": "2024-02-01T10:30:00",
-                      "endTime": "2024-02-01T11:30:00"
-                    }
-                  ]
-                },
-                {
-                  "schedule": [
-                    {
-                      "startTime": "2024-02-02T10:30:00",
-                      "endTime": "2024-02-02T11:30:00"
-                    }
-                  ]
-                },
-                {
-                  "schedule": [
-                    {
-                      "startTime": "2024-02-03T10:30:00",
-                      "endTime": "2024-02-03T11:30:00"
-                    }
-                  ]
-                }
-              ]
-            """)
+                      [
+                        {
+                          "schedule": [
+                            {
+                              "startTime": "2024-01-31T17:00:00",
+                              "endTime": "2024-01-31T20:30:00"
+                            },
+                            {
+                              "startTime": "2024-01-31T21:30:00",
+                              "endTime": "2024-01-31T22:00:00"
+                            }
+                          ]
+                        },
+                        {
+                          "schedule": [
+                            {
+                              "startTime": "2024-02-01T10:30:00",
+                              "endTime": "2024-02-01T11:30:00"
+                            }
+                          ]
+                        },
+                        {
+                          "schedule": [
+                            {
+                              "startTime": "2024-02-02T10:30:00",
+                              "endTime": "2024-02-02T11:30:00"
+                            }
+                          ]
+                        },
+                        {
+                          "schedule": [
+                            {
+                              "startTime": "2024-02-03T10:30:00",
+                              "endTime": "2024-02-03T11:30:00"
+                            }
+                          ]
+                        }
+                      ]
+                    """)
+    @NotEmpty(message = "스케줄은 적어도 하나 이상의 값을 넣어야 저장할 수 있습니다.")
     private List<ScheduleDayRequest> schedule;
 
     public ScheduleCreateUpdateRequest(List<ScheduleDayRequest> schedule) {
