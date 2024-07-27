cef9fd7aaf852e6bde0e010db0aeb4317f86bb2d
김정아 <92552047+squidjiny@users.noreply.github.com>
2024-07-27 18:57:01 +0900

    feat: schedule 관련 유효성 검사 강화: startTime과 endTime이 null일 때도 유효성 검사를 통과하지 못하도록 수정#119

--- timepiece/src/main/java/com/appcenter/timepiece/dto/schedule/ScheduleDto.java
+++ timepiece/src/main/java/com/appcenter/timepiece/dto/schedule/ScheduleDto.java
@@ -1,6 +1,7 @@
 package com.appcenter.timepiece.dto.schedule;
 
 import com.appcenter.timepiece.domain.Schedule;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -8,8 +9,10 @@ import java.time.LocalDateTime;
 @Getter
 public class ScheduleDto {
 
+    @NotNull(message = "프로젝트 시작 날짜를 입력 해 주세요.")
     private LocalDateTime startTime;
 
+    @NotNull(message = "프로젝트 마지막 날짜를 입력 해 주세요.")
     private LocalDateTime endTime;
 
     public ScheduleDto(LocalDateTime startTime, LocalDateTime endTime) {
