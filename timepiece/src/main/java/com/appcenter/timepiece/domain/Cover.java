d8c42971409fad2fedc5987f114f19b36aed765f
jang-namu <ironhee8005@gmail.com>
2024-07-28 15:49:07 +0900

    refactor: Cover Entity 변경, 커버는 thumbnail-cover 필드를 갖는다. - Cover 엔티티 thumbnailUrl 속성 추가 - CoverDataResponse(메타데이터 조회 API) 로직 Fix

--- timepiece/src/main/java/com/appcenter/timepiece/domain/Cover.java
+++ timepiece/src/main/java/com/appcenter/timepiece/domain/Cover.java
@@ -17,17 +17,21 @@ public class Cover {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "thumbnail_url", columnDefinition = "TEXT")
+    private String thumbnailUrl;
+
     @Column(name = "cover_image_url", columnDefinition = "TEXT")
     private String coverImageUrl;
 
     @OneToMany(mappedBy = "cover")
     private List<Project> projects = new ArrayList<>();
 
-    private Cover(String coverImageUrl) {
+    private Cover(String thumbnailUrl, String coverImageUrl) {
+        this.thumbnailUrl = thumbnailUrl;
         this.coverImageUrl = coverImageUrl;
     }
 
-    public static Cover of(String coverImageUrl) {
-        return new Cover(coverImageUrl);
+    public static Cover of(String thumbnailUrl, String coverImageUrl) {
+        return new Cover(thumbnailUrl, coverImageUrl);
     }
 }
