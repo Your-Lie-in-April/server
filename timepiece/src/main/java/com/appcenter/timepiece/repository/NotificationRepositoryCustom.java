6b60aa28e844f086655151fc40ef2c2bb4819e30
jang-namu <ironhee8005@gmail.com>
2025-02-02 17:01:57 +0900

    fix: SSE 재연결 시 유실된 알림 재전송

--- timepiece/src/main/java/com/appcenter/timepiece/repository/NotificationRepositoryCustom.java
+++ timepiece/src/main/java/com/appcenter/timepiece/repository/NotificationRepositoryCustom.java
@@ -1,12 +1,18 @@
 package com.appcenter.timepiece.repository;
 
 import com.appcenter.timepiece.domain.Notification;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
 
 public interface NotificationRepositoryCustom {
-    List<Notification> findAllByTimestampAfter(Long receiverId, LocalDateTime timestamp, Boolean isChecked, int pageSize);
-    List<Notification> findAllByTimestampAfter(Long receiverId, Long projectId, LocalDateTime timestamp, Boolean isChecked, int pageSize);
+    List<Notification> findAllByTimestampAfter(Long receiverId, LocalDateTime timestamp, Boolean isChecked,
+                                               int pageSize);
+
+    List<Notification> findAllByTimestampAfter(Long receiverId, Long projectId, LocalDateTime timestamp,
+                                               Boolean isChecked, int pageSize);
+
+    List<Notification> findAllByReceiverLargerThanNotificationId(Long receiverId, Long notificationId);
+
+    List<Notification> findAllByReceiverLargerThanNotificationId(Long receiverId, Long projectId, Long notificationId);
 }
