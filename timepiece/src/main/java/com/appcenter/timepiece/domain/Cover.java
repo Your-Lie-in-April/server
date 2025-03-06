20c9f1b66b4ca7c1be87c3bd90a3ef643a0a2b52
jang-namu <ironhee8005@gmail.com>
2025-03-06 15:05:24 +0900

    refactor: Cover 엔티티가 BaseTimeEntity를 상속

--- timepiece/src/main/java/com/appcenter/timepiece/domain/Cover.java
+++ timepiece/src/main/java/com/appcenter/timepiece/domain/Cover.java
@@ -1,5 +1,6 @@
 package com.appcenter.timepiece.domain;
 
+import com.appcenter.timepiece.common.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,7 +13,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Cover {
+public class Cover extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
