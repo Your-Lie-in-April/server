79af82a169d90f359982ddd0ad4c4cbc087ad4e3
jang-namu <ironhee8005@gmail.com>
2025-03-07 23:33:26 +0900

    refactor: @EnableJpaAuditing Configuration 파일 분리
    - 슬라이싱 테스트 수행 시 Jpa 메타모델로 인한 이슈 해결

--- timepiece/src/main/java/com/appcenter/timepiece/TimepieceApplication.java
+++ timepiece/src/main/java/com/appcenter/timepiece/TimepieceApplication.java
@@ -7,11 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableConfigurationProperties(CorsProperties.class)
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server url")})
-@EnableJpaAuditing
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class TimepieceApplication {
