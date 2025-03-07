3c2982d958267387f38aa5d1478d0a682d6f83da
jang-namu <ironhee8005@gmail.com>
2025-03-08 00:17:43 +0900

    refactor: dto 생성자 변경

--- timepiece/src/main/java/com/appcenter/timepiece/dto/notify/NotificationResponse.java
+++ timepiece/src/main/java/com/appcenter/timepiece/dto/notify/NotificationResponse.java
@@ -1,13 +1,14 @@
 package com.appcenter.timepiece.dto.notify;
 
 import com.appcenter.timepiece.domain.Member;
+import com.appcenter.timepiece.domain.MemberProject;
 import com.appcenter.timepiece.domain.Notification;
 import com.appcenter.timepiece.domain.Project;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-
-import java.time.LocalDateTime;
 
 @Getter
 public class NotificationResponse {
@@ -40,7 +41,9 @@ public class NotificationResponse {
         }
 
         public static ProjectInfo from(Project project) {
-            if (project == null) return null;
+            if (project == null) {
+                return null;
+            }
             return new ProjectInfo(project.getId(), project.getTitle());
         }
     }
@@ -56,14 +59,26 @@ public class NotificationResponse {
             this.nickname = nickname;
         }
 
-        public static MemberInfo from(Member member) {
-            if (member == null) return null;
+        public static MemberInfo fromMemberProject(MemberProject memberProject) {
+            if (memberProject == null) {
+                return null;
+            }
+            return new MemberInfo(memberProject.getMemberId(), memberProject.getNickname());
+        }
+
+        public static MemberInfo fromMember(Member member) {
+            if (member == null) {
+                return null;
+            }
             return new MemberInfo(member.getId(), member.getNickname());
         }
     }
 
+    @JsonCreator
     @Builder(access = AccessLevel.PRIVATE)
-    public NotificationResponse(Long notificationId, String message, ProjectInfo project, MemberInfo receiver, MemberInfo sender, Notification.NotificationType type, Boolean isChecked, LocalDateTime createdAt) {
+    public NotificationResponse(Long notificationId, String message, ProjectInfo project, MemberInfo receiver,
+                                MemberInfo sender, Notification.NotificationType type, Boolean isChecked,
+                                LocalDateTime createdAt) {
         this.notificationId = notificationId;
         this.message = message;
         this.project = project;
@@ -74,13 +89,29 @@ public class NotificationResponse {
         this.createdAt = createdAt;
     }
 
-    public static NotificationResponse from(Notification notification) {
+    public NotificationResponse(Notification notification, Project project, MemberProject sender,
+                                MemberProject receiver
+    ) {
+        this.notificationId = notification.getId();
+        this.message = notification.getMessage();
+        this.project = ProjectInfo.from(project);
+        this.receiver = MemberInfo.fromMemberProject(receiver);
+        this.sender = MemberInfo.fromMemberProject(sender);
+        this.type = notification.getType();
+        this.isChecked = notification.getIsChecked();
+        this.createdAt = notification.getCreatedAt();
+    }
+
+
+    public static NotificationResponse from(Notification notification, Project project, MemberProject sender,
+                                            MemberProject receiver
+    ) {
         return NotificationResponse.builder()
                 .notificationId(notification.getId())
                 .message(notification.getMessage())
-                .project(ProjectInfo.from(notification.getProject()))
-                .receiver(MemberInfo.from(notification.getReceiver()))
-                .sender(MemberInfo.from(notification.getSender()))
+                .project(ProjectInfo.from(project))
+                .receiver(MemberInfo.fromMemberProject(receiver))
+                .sender(MemberInfo.fromMemberProject(sender))
                 .type(notification.getType())
                 .isChecked(notification.getIsChecked())
                 .createdAt(notification.getCreatedAt())
