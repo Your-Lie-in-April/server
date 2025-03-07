65a79cad6ff1a86a52d13f098d2633a7ad1472f3
jang-namu <ironhee8005@gmail.com>
2025-03-08 00:09:55 +0900

    refactor: 미사용 메서드 삭제

--- timepiece/src/main/java/com/appcenter/timepiece/repository/CoverRepository.java
+++ timepiece/src/main/java/com/appcenter/timepiece/repository/CoverRepository.java
@@ -4,10 +4,7 @@ import com.appcenter.timepiece.domain.Cover;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface CoverRepository extends JpaRepository<Cover, Long> {
 
-    Optional<Cover> findByCoverImageUrl(String coverImageUrl);
 }
