12110e91d26035595d689028eb0327c12f69d285
jang-namu <ironhee8005@gmail.com>
2025-03-08 00:13:26 +0900

    refactor: 연관관계 매핑 제거, Repository 수정

--- timepiece/src/main/java/com/appcenter/timepiece/repository/NotificationRepositoryCustom.java
+++ timepiece/src/main/java/com/appcenter/timepiece/repository/NotificationRepositoryCustom.java
@@ -1,6 +1,7 @@
 package com.appcenter.timepiece.repository;
 
 import com.appcenter.timepiece.domain.Notification;
+import com.appcenter.timepiece.dto.notify.NotificationResponse;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -15,4 +16,9 @@ public interface NotificationRepositoryCustom {
     List<Notification> findAllByReceiverLargerThanNotificationId(Long receiverId, Long notificationId);
 
     List<Notification> findAllByReceiverLargerThanNotificationId(Long receiverId, Long projectId, Long notificationId);
+
+    List<NotificationResponse> finaAllByReceiverId(Long receiverId, LocalDateTime cursor, Boolean isChecked, int size);
+
+    List<NotificationResponse> finaAllByReceiverIdInProject(Long receiverId, Long projectId, LocalDateTime cursor,
+                                                            Boolean isChecked, int size);
 }
