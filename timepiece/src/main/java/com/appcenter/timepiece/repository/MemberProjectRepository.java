409b61748ebee22146df48907f1aecbaa0680bba
jang-namu <ironhee8005@gmail.com>
2025-03-08 00:10:51 +0900

    refactor: 연관관계 제거, 프로젝트 목록 조회 프로젝션 사용토록 변경

--- timepiece/src/main/java/com/appcenter/timepiece/repository/MemberProjectRepository.java
+++ timepiece/src/main/java/com/appcenter/timepiece/repository/MemberProjectRepository.java
@@ -1,12 +1,22 @@
 package com.appcenter.timepiece.repository;
 
 import com.appcenter.timepiece.domain.MemberProject;
-import java.util.Optional;
+import com.appcenter.timepiece.dto.project.ProjectThumbnailResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberProjectRepository extends JpaRepository<MemberProject, Long>, MemberProjectRepositoryCustom {
 
-    Optional<MemberProject> findByMemberId(Long memberId);
+    @Query("SELECT new com.appcenter.timepiece.dto.project.ProjectThumbnailResponse(p, c.thumbnailUrl) " +
+            "FROM Project p " +
+            "INNER JOIN MemberProject mp ON mp.projectId = p.id " +
+            "LEFT JOIN Cover c ON p.coverId = c.id " +
+            "WHERE mp.memberId = :memberId " +
+            "AND p.isDeleted = false AND mp.isStored = false " +
+            "ORDER BY p.updatedAt DESC")
+    Page<ProjectThumbnailResponse> fetchProjectThumbnailResponse(Pageable pageable, Long memberId);
 }
