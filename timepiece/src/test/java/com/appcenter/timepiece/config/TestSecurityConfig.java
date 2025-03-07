62815aab96412861021087b6fbb3e79bbdfae131
jang-namu <ironhee8005@gmail.com>
2025-03-07 23:33:43 +0900

    test: 테스트용 SecurityConfig

---
+++ timepiece/src/test/java/com/appcenter/timepiece/config/TestSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.appcenter.timepiece.config;
+
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.Collections;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.SecurityFilterChain;
+
+@TestConfiguration
+public class TestSecurityConfig {
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .httpBasic(HttpBasicConfigurer::disable)
+                .csrf(CsrfConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                )
+                .exceptionHandling(authentication -> authentication
+                        .authenticationEntryPoint((request, response, exception) ->
+                                response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                        .accessDeniedHandler((request, response, exception) ->
+                                response.sendError(HttpServletResponse.SC_FORBIDDEN))
+                );
+
+        return httpSecurity.build();
+    }
+
+    // 필요시 테스트용 인증 관리자 추가
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        return authentication -> {
+            // 테스트용 인증 로직 (항상 인증 성공 또는 특정 조건에 따라 인증)
+            return new UsernamePasswordAuthenticationToken("testuser", "password",
+                    Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
+        };
+    }
+}
