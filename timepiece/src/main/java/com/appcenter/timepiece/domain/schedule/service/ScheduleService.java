14339f80dda8ed706014b4149e4f45737950d5b8
jang-namu <ironhee8005@gmail.com>
2025-03-18 01:48:26 +0900

    comment: 주석 정리

--- timepiece/src/main/java/com/appcenter/timepiece/domain/schedule/service/ScheduleService.java
+++ timepiece/src/main/java/com/appcenter/timepiece/domain/schedule/service/ScheduleService.java
@@ -39,22 +39,17 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ScheduleService {
 
-    private final MemberProjectRepository memberProjectRepository;
-    private final ScheduleRepository scheduleRepository;
-    private final ProjectRepository projectRepository;
     private final NotificationService notificationService;
+    private final MemberProjectRepository memberProjectRepository;
     private final MemberRepository memberRepository;
+    private final ProjectRepository projectRepository;
+    private final ScheduleRepository scheduleRepository;
     private final ScheduleValidator scheduleValidator;
 
     /**
      * {@summary 프로젝트 내 모든 멤버의 스케줄을 조회한다(본인포함)}
      * <p>자신을 포함한 모든 멤버의 스케줄을 조회한다.
      * 모든 프로젝트 멤버의 스케줄을 전체 조회한 후,스케줄 중 멤버별로 중복되는 요일을 필터링, 자신의 스케줄을 필터링하여 요일별로 묶어 반환한다. </p>
-     *
-     * @param projectId
-     * @param condition
-     * @param userDetails
-     * @return
      */
     @Transactional(readOnly = true)
     public List<ScheduleWeekResponse> findMembersSchedules(Long projectId, LocalDate condition,
@@ -108,12 +103,6 @@ public class ScheduleService {
      * {@summary 특정 프로젝트-사용자의 condition 날짜가 포함된 주차 스케줄 조회}
      * <p>condition이 속한 주차(= 해당 주차의 일요일 ~ 토요일까지)의 사용자 스케줄을 조회한다.
      * 해당 기간동안의 사용자의 스케줄을 전부 조회한 후 날짜별로 묶어 반환한다.
-     *
-     * @param projectId
-     * @param memberId
-     * @param condition
-     * @param userDetails
-     * @return
      */
     @Transactional(readOnly = true)
     public ScheduleWeekResponse findSchedule(Long projectId, Long memberId, LocalDate condition,
@@ -132,7 +121,6 @@ public class ScheduleService {
                 scheduleCollection.toScheduleDayResponses());
     }
 
-    // todo: ProjectService와 중복코드
     private void validateMemberIsInProject(Long projectId, UserDetails userDetails) {
         Long memberId = ((CustomUserDetails) userDetails).getId();
         boolean isExist = memberProjectRepository.existsByMemberIdAndProjectId(memberId, projectId);
@@ -141,13 +129,6 @@ public class ScheduleService {
         }
     }
 
-    /**
-     * {@summary 스케줄 생성}
-     *
-     * @param request
-     * @param projectId
-     * @param userDetails
-     */
     @Transactional
     public void createSchedule(ScheduleCreateUpdateRequest request, Long projectId, UserDetails userDetails) {
         Project project = projectRepository.findById(projectId)
@@ -176,10 +157,6 @@ public class ScheduleService {
      * {@summary 기존 스케줄 삭제 후 새 스케줄 저장}
      * <p>request에 존재하는 첫번째 스케줄의 날짜로, 해당 주차의 첫번째 요일(일요일)을 계산,
      * 계산한 첫번째 요일 ~ (첫번째 요일 + 7)일의 기간에 속하는 스케줄을 DB에서 삭제하고 request로 받은 수정 후 스케줄을 저장
-     *
-     * @param request
-     * @param projectId
-     * @param userDetails
      */
     @Transactional
     public void editSchedule(ScheduleCreateUpdateRequest request, Long projectId, UserDetails userDetails) {
@@ -191,7 +168,6 @@ public class ScheduleService {
         MemberProject memberProject = memberProjectRepository.findByMemberIdAndProjectId(memberId, projectId)
                 .orElseThrow(() -> new NotFoundElementException(ExceptionMessage.MEMBER_PROJECT_NOT_FOUND));
 
-        // todo: IndexOutOfBoundsException!! 발생 가능 -> Not Null, Not Empty하면 될 듯?
         LocalDateTime sundayOfWeek = calculateStartDay(
                 request.getSchedule().get(0).getSchedule().get(0).getStartTime());
         scheduleRepository.deleteMemberSchedulesBetween(memberProject.getId(), sundayOfWeek,
@@ -213,10 +189,6 @@ public class ScheduleService {
 
     /**
      * {@summary start <= 날짜 < end}에 속하는 스케줄 삭제
-     *
-     * @param request
-     * @param projectId
-     * @param userDetails
      */
     @Transactional
     public void deleteSchedule(ScheduleDeleteRequest request, Long projectId, UserDetails userDetails) {
